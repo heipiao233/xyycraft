7e9fde616b44596503e074207aabbcc13012da74
heipiao233 <heipiao233@outlook.com>
2021-06-18 19:22:37 +0800

    翻译、创造标签页

--- src/main/java/net/heipiao/xyycraft/item/Items.java
+++ src/main/java/net/heipiao/xyycraft/item/Items.java
@@ -1,12 +1,13 @@
 package net.heipiao.xyycraft.item;
 
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 
 public class Items {
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, "xyycraft");
-    public static final RegistryObject<Item> ICE_GUN = ITEMS.register("ice_gun", ()->new IceGunItem(new Item.Properties().durability(2)));
+    public static final RegistryObject<Item> ICE_GUN = ITEMS.register("ice_gun", ()->new IceGunItem(new Item.Properties().durability(2).tab(ItemGroup.TAB_COMBAT)));
     public static final RegistryObject<Item> ICE_CHARGE = ITEMS.register("ice_charge", ()->new Item(new Item.Properties()));
 }
