9b4acb941bbb571201e73ceadb14691824354676
heipiao233 <heipiao233@outlook.com>
2021-06-29 09:36:26 +0800

    Removed bbmodels

---
+++ src/main/java/net/heipiao/xyycraft/item/BraveBeltItem.java
@@ -0,0 +1,9 @@
+package net.heipiao.xyycraft.item;
+
+import net.minecraft.item.Item;
+
+public class BraveBeltItem extends Item{
+    public BraveBeltItem(Item.Properties properties) {
+        super(properties);
+    }
+}
--- src/main/java/net/heipiao/xyycraft/item/Items.java
+++ src/main/java/net/heipiao/xyycraft/item/Items.java
@@ -1,11 +1,9 @@
 package net.heipiao.xyycraft.item;
 
-import net.minecraft.inventory.EquipmentSlotType;
-import net.minecraft.item.ArmorItem;
-import net.minecraft.item.ArmorMaterial;
-import net.minecraft.item.Food;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemTier;
+import net.minecraft.item.SwordItem;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -15,5 +13,5 @@ public class Items {
     public static final RegistryObject<Item> ICE_GUN = ITEMS.register("ice_gun", ()->new IceGunItem(new Item.Properties().tab(ItemGroup.TAB_COMBAT)));
     public static final RegistryObject<Item> ICE_CHARGE = ITEMS.register("ice_charge", ()->new Item(new Item.Properties()));
     public static final RegistryObject<Item> SPATULA = ITEMS.register("spatula", ()->new SpatulaItem(new Item.Properties().tab(ItemGroup.TAB_TOOLS)));
-    public static final RegistryObject<Item> BRAVE_BELT = ITEMS.register("brave_belt", ()->new ArmorItem(ArmorMaterial.NETHERITE, EquipmentSlotType.LEGS, new Item.Properties().tab(ItemGroup.TAB_FOOD).food(new Food.Builder().nutrition(20).saturationMod(1).build())));
+    public static final RegistryObject<Item> BRAVE_BELT = ITEMS.register("brave_belt", ()->new SwordItem(ItemTier.NETHERITE, 20, 0, new Item.Properties().tab(ItemGroup.TAB_COMBAT)));
 }
